15bbbf680f3103cc681bb0e9e98485eefdf604ae
HirokazuBA <h.baba@tworks.co.jp>
2024-07-04 16:22:54 +0900

    add_RadioButton

--- chrome-extension/popup.js
+++ chrome-extension/popup.js
@@ -1,28 +1,33 @@
 document.addEventListener("DOMContentLoaded", function () {
   const orderForm = document.getElementById("orderForm");
-  const tradeTypeSelect = document.getElementById("tradeType");
+  const tradeTypeRadios = document.getElementsByName("tradeType");
+  const orderTypeRadios = document.getElementsByName("orderType");
   const priceInput = document.getElementById("price");
 
   // Trade Typeが変更されたときにpriceフィールドの有効/無効を切り替える
-  tradeTypeSelect.addEventListener("change", function () {
-    if (tradeTypeSelect.value === "成行") {
-      priceInput.disabled = true;
-      priceInput.value = ""; // 成行の場合、priceフィールドをクリア
-      // errorMessage.textContent = "";
-    } else {
-      priceInput.disabled = false;
-    }
+  tradeTypeRadios.forEach((radio) => {
+    radio.addEventListener("change", function () {
+      if (radio.value === "成行") {
+        priceInput.disabled = true;
+        priceInput.value = ""; // 成行の場合、priceフィールドをクリア
+      } else {
+        priceInput.disabled = false;
+      }
+    });
   });
 
   orderForm.addEventListener("submit", function (event) {
     event.preventDefault();
 
+    const orderType = document.querySelector('input[name="orderType"]:checked');
+    const tradeType = document.querySelector('input[name="tradeType"]:checked');
+
     const orderData = {
       accountNumber: document.getElementById("accountNumber").value,
       stockName: document.getElementById("stockName").value,
-      orderType: document.getElementById("orderType").value,
+      orderType: orderType ? orderType.value : "",
       quantity: document.getElementById("quantity").value,
-      tradeType: tradeTypeSelect.value,
+      tradeType: tradeType ? tradeType.value : "",
       price: priceInput.value,
     };
 
@@ -37,5 +42,5 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 
   // 初期ロード時にtradeTypeの値に基づいてpriceフィールドを設定
-  tradeTypeSelect.dispatchEvent(new Event("change"));
+  tradeTypeRadios.forEach((radio) => radio.dispatchEvent(new Event("change")));
 });
